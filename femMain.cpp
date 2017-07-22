0048d54d7f886abe79af4209100855bb190105e1
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2017-07-22 10:51:09 +0900

    コミット忘れ

--- femMain.cpp
+++ femMain.cpp
@@ -6,13 +6,10 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    cout<<argc<<endl;
     
     Driver driver;
 
-
     driver.fileDir_=argv[1];
-        cout<<driver.fileDir_<<endl;
     
     driver.readInputFiles();
     
